dfafd16f13c637a6e4964f21c7f64969bdbec779
Diego Cambiaso <diegocambiaso@gmail.com>
2023-04-17 15:46:31 -0500

    Created a vlc-video-player.py

---
+++ vlc-video-player.py
@@ -0,0 +1,15 @@
+import vlc
+import time
+
+player = vlc.Instance()
+media_list = player.media_list_new()
+media_player = player.media_list_player_new()
+
+# creating a new media path
+media = player.media_new_path("video.mp4")
+
+media_list.add_media(media)
+media_player.set_media_list(media_list)
+media_player.play()
+
+time.sleep(5)
